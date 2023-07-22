38d1710942ffd0be08ffe598034e454e6d7c3b57
Ankit Sahu <ankitsahu2032@gmail.com>
2023-07-22 12:20:12 +0530

    Create SortAnArrayOf0's1'sAnd2's.cpp

---
+++ Arrays/SortAnArrayOf0's1'sAnd2's.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        // Dutch National Flag Algorithm 
+        int n=nums.size();
+        int low=0, mid=0, high=n-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+                mid++;
+            else
+            {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};

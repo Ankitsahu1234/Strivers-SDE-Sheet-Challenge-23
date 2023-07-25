851f09118cee39817d7b70ecd4006032c431f542
Ankit Sahu <ankitsahu2032@gmail.com>
2023-07-25 14:28:50 +0530

    Update MergeTwoSortedArrays.cpp

--- Arrays Part-II/MergeTwoSortedArrays.cpp
+++ Arrays Part-II/MergeTwoSortedArrays.cpp
@@ -2,10 +2,15 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        for(int i=0; i<n; i++)
+        int left=m-1;
+        int right=n-1;
+        int i=m+n-1;
+        while(right>=0)
         {
-            nums1[m+i]=nums2[i];
+            if(left>=0 && nums1[left]>nums2[right])
+                nums1[i--]=nums1[left--];
+            else
+                nums1[i--]=nums2[right--];
         }
-        sort(nums1.begin(), nums1.end());
     }
 };

b701640829d9d9755f930d96f56f5649befabecb
Ankit Sahu <ankitsahu2032@gmail.com>
2023-07-25 21:27:47 +0530

    Update CountInversions.cpp

--- Arrays Part-II/CountInversions.cpp
+++ Arrays Part-II/CountInversions.cpp
@@ -1,18 +1,54 @@
 #include <bits/stdc++.h> 
 
-long long getInversions(long long *arr, int n){
-    // Write your code here.
-  
-    int ans=0;
-    for(int i=0; i<n-1; i++)
+long long merge(long long *arr, int low, int mid, int high)
+{
+    vector<int> temp;
+    int left=low;
+    int right=mid+1;
+    long long cnt=0;
+    while(left<=mid && right<=high)
     {
-        for(int j=i+1; j<n; j++)
+        if(arr[left]<=arr[right])
         {
-            if(arr[i]>arr[j])
-            {
-                ans++;
-            }
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            cnt+=(mid-left+1);
+            right++;
         }
     }
-    return ans;
+    while(left<=mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i=low; i<=high; i++)
+    {
+        arr[i]=temp[i-low];
+    }
+    return cnt;
+}
+long long mergeSort(long long *arr, int low, int high)
+{
+    long long cnt=0;
+    if(low>=high)
+        return 0;
+    int mid=(low+high)/2;
+    cnt+=mergeSort(arr, low, mid);
+    cnt+=mergeSort(arr, mid+1, high);
+    cnt+=merge(arr, low, mid, high);
+    return cnt;
+}
+long long getInversions(long long *arr, int n){
+    // Write your code here.
+
+    return mergeSort(arr, 0, n-1);
 }

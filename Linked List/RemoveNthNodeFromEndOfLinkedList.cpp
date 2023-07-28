ae492ad6730d3035de19d3bb088f4c121acb5afd
Ankit Sahu <ankitsahu2032@gmail.com>
2023-07-28 23:25:08 +0530

    Update RemoveNthNodeFromEndOfLinkedList.cpp

--- Linked List/RemoveNthNodeFromEndOfLinkedList.cpp
+++ Linked List/RemoveNthNodeFromEndOfLinkedList.cpp
@@ -2,31 +2,55 @@ class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         
-        if(head->next==NULL)
-            return NULL;
-        int len=0;
-        ListNode *curr=head;
-        while(curr!=NULL)
+        // Method 2: Efficient: 2 Pointer Approach
+
+        ListNode *start=new ListNode();
+        start->next=head;
+        ListNode *slow=start;
+        ListNode *fast=start;
+        for(int i=1; i<=n; i++)
         {
-            curr=curr->next;
-            len++;
+            fast=fast->next;
         }
-        ListNode *prev=NULL;
-        curr=head;
-        if(n==len)
+        while(fast->next!=NULL)
         {
-            curr=curr->next;
-            delete head;
-            return curr;
+            slow=slow->next;
+            fast=fast->next;
         }
-        int k=len-n;
-        while(k--)
-        {
-            prev=curr;
-            curr=curr->next;
-        }
-        prev->next=curr->next;
-        delete (curr);
-        return head;
+        slow->next=slow->next->next;
+        return start->next;
     }
 };
+
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+//         if(head->next==NULL)
+//             return NULL;
+//         int len=0;
+//         ListNode *curr=head;
+//         while(curr!=NULL)
+//         {
+//             curr=curr->next;
+//             len++;
+//         }
+//         ListNode *prev=NULL;
+//         curr=head;
+//         if(n==len)
+//         {
+//             curr=curr->next;
+//             delete head;
+//             return curr;
+//         }
+//         int k=len-n;
+//         while(k--)
+//         {
+//             prev=curr;
+//             curr=curr->next;
+//         }
+//         prev->next=curr->next;
+//         delete (curr);
+//         return head;
+//     }
+// };

a5a88ace073a966feb3646a93633a3dc34b9459b
Ankit Sahu <ankitsahu2032@gmail.com>
2023-07-28 23:14:18 +0530

    Update MergeTwoSortedLinkedList.cpp

--- Linked List/MergeTwoSortedLinkedList.cpp
+++ Linked List/MergeTwoSortedLinkedList.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
 
-    ListNode* merge(ListNode *list1, ListNode *list2)
+    ListNode* merge(ListNode *l1, ListNode *l2)
     {
-        if(list1==NULL)
-            return list2;
-        if(list2==NULL)
-            return list1;
-        vector<ListNode*> temp;
-        ListNode *curr1=list1, *curr2=list2;
-        while(curr1!=NULL && curr2!=NULL)
+        // Method 2: Inplace method without using extra space
+
+        if(l1==NULL)
+            return l2;
+        if(l2==NULL)
+            return l1;
+        if(l1->val > l2->val)
+            swap(l1, l2);
+        ListNode *res=l1;
+        while(l1!=NULL && l2!=NULL)
         {
-            if(curr1->val <= curr2->val)
+            ListNode *temp=NULL;
+            while(l1!=NULL && l1->val <= l2->val)
             {
-                temp.push_back(curr1);
-                curr1=curr1->next;
+                temp=l1;
+                l1=l1->next;
             }
-            else
-            {
-                temp.push_back(curr2);
-                curr2=curr2->next;
-            }
+            temp->next=l2;
+            swap(l1, l2);
         }
-        while(curr1!=NULL)
-        {
-            temp.push_back(curr1);
-            curr1=curr1->next;
-        }
-        while(curr2!=NULL)
-        {
-            temp.push_back(curr2);
-            curr2=curr2->next;
-        }
-        ListNode *head=temp[0];
-        ListNode *curr=head;
-        for(int i=1; i<temp.size(); i++)
-        {
-            curr->next=temp[i];
-            curr=curr->next;
-        }
-        return head;
+        return res;
     }
 
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {

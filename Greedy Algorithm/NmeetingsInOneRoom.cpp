5d572c0c6d6a0dbc4b93a26fa4c3fb4a72cf8241
Ankit Sahu <ankitsahu2032@gmail.com>
2023-08-01 15:51:42 +0530

    Update NmeetingsInOneRoom.cpp

--- Greedy Algorithm/NmeetingsInOneRoom.cpp
+++ Greedy Algorithm/NmeetingsInOneRoom.cpp
@@ -1,28 +1,61 @@
-bool myCmp(pair<int,int> &a, pair<int,int> &b)
+class Solution
 {
-    return a.second<b.second;
-}
-
-int maximumMeetings(vector<int> &start, vector<int> &end)
-{
-    // Write your code here.
-
-    int n=start.size();
-    vector<pair<int, int>> temp;
-    int res=1;
-    for(int i=0; i<n; i++)
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int start[], int end[], int n)
     {
-        temp.push_back(make_pair(start[i], end[i]));
-    }
-    sort(temp.begin(), temp.end(), myCmp);
-    int prevEnd=temp[0].second;
-    for(int i=1; i<temp.size(); i++)
-    {
-        if(prevEnd<temp[i].first)
+        // Your code here
+        
+        // Method 4: Using Priority Queue (Min Heap)
+        
+        priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        for(int i=0; i<n; i++)
         {
-            res++;
-            prevEnd=temp[i].second;
+            pq.push({end[i], start[i]});
         }
+        int prevEnd=pq.top().first;
+        pq.pop();
+        int count=1;
+        while(!pq.empty())
+        {
+            auto it=pq.top();
+            pq.pop();
+            if(it.second > prevEnd)
+            {
+                count++;
+                prevEnd=it.first;
+            }
+        }
+        return count;
     }
-    return res;
-}
+};
+
+// bool myCmp(pair<int,int> &a, pair<int,int> &b)
+// {
+//     return a.second<b.second;
+// }
+// int maximumMeetings(vector<int> &start, vector<int> &end)
+// {
+//     // Write your code here.
+
+//     // Method 1:
+//     int n=start.size();
+//     vector<pair<int, int>> temp;
+//     int res=1;
+//     for(int i=0; i<n; i++)
+//     {
+//         temp.push_back(make_pair(start[i], end[i]));
+//     }
+//     sort(temp.begin(), temp.end(), myCmp);
+//     int prevEnd=temp[0].second;
+//     for(int i=1; i<temp.size(); i++)
+//     {
+//         if(prevEnd<temp[i].first)
+//         {
+//             res++;
+//             prevEnd=temp[i].second;
+//         }
+//     }
+//     return res;
+// }

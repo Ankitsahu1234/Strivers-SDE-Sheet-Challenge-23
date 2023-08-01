285a46a7511e30ba1657295e2c52b0327c1dcbdc
Ankit Sahu <ankitsahu2032@gmail.com>
2023-08-01 14:28:47 +0530

    Create RemoveDuplicatesfromSortedArray.cpp

---
+++ Linked List and Arrays/RemoveDuplicatesfromSortedArray.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        int n=nums.size();
+        if(n==1)
+            return n;
+        vector<int> temp;
+        bool flag=false;
+        for(int i=0; i<n-1; i++)
+        {
+            if(nums[i]!=nums[i+1])
+            {
+                flag=true;
+                temp.push_back(nums[i]);
+            }
+            if(i==n-2 && nums[n-2]==nums[n-1])
+                temp.push_back(nums[i]);
+        }
+        if(flag==false)
+            return 1;
+        if(nums[n-1]!=nums[n-2])
+            temp.push_back(nums[n-1]);
+        for(int i=0; i<temp.size(); i++)
+        {
+            nums[i]=temp[i];
+        }
+        return temp.size();
+    }
+};

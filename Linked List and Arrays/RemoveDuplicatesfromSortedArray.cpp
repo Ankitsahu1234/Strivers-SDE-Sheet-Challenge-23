d1b19d47c3cff51fd4546c96860c3735c9ec75e8
Ankit Sahu <ankitsahu2032@gmail.com>
2023-08-01 14:44:14 +0530

    Update RemoveDuplicatesfromSortedArray.cpp

--- Linked List and Arrays/RemoveDuplicatesfromSortedArray.cpp
+++ Linked List and Arrays/RemoveDuplicatesfromSortedArray.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+
+        // Method 2:
         int n=nums.size();
-        if(n==1)
-            return n;
-        vector<int> temp;
-        bool flag=false;
-        for(int i=0; i<n-1; i++)
-        {
-            if(nums[i]!=nums[i+1])
-            {
-                flag=true;
-                temp.push_back(nums[i]);
-            }
-            if(i==n-2 && nums[n-2]==nums[n-1])
-                temp.push_back(nums[i]);
-        }
-        if(flag==false)
-            return 1;
-        if(nums[n-1]!=nums[n-2])
-            temp.push_back(nums[n-1]);
-        for(int i=0; i<temp.size(); i++)
-        {
-            nums[i]=temp[i];
-        }
-        return temp.size();
+        auto it=unique(nums.begin(), nums.end());
+        return it-nums.begin();
     }
 };
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+
+//         // Method 1:
+//         int n=nums.size();
+//         if(n==1)
+//             return n;
+//         vector<int> temp;
+//         bool flag=false;
+//         for(int i=0; i<n-1; i++)
+//         {
+//             if(nums[i]!=nums[i+1])
+//             {
+//                 flag=true;
+//                 temp.push_back(nums[i]);
+//             }
+//             if(i==n-2 && nums[n-2]==nums[n-1])
+//                 temp.push_back(nums[i]);
+//         }
+//         if(flag==false)
+//             return 1;
+//         if(nums[n-1]!=nums[n-2])
+//             temp.push_back(nums[n-1]);
+//         for(int i=0; i<temp.size(); i++)
+//         {
+//             nums[i]=temp[i];
+//         }
+//         return temp.size();
+//     }
+// };

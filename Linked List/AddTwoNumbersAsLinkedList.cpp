23e8d8ee8a9e31419cac02238ba550a1cd23dc33
Ankit Sahu <ankitsahu2032@gmail.com>
2023-08-05 18:49:24 +0530

    Update AddTwoNumbersAsLinkedList.cpp

--- Linked List/AddTwoNumbersAsLinkedList.cpp
+++ Linked List/AddTwoNumbersAsLinkedList.cpp
@@ -2,43 +2,28 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-        ListNode *curr1=l1, *curr2=l2;
-        ListNode *head=new ListNode();
-        ListNode *prev=head;
-        int rem=0; 
-        int sum=0;
-        while(curr1!=NULL || curr2!=NULL)
-        {   
-            ListNode *temp=new ListNode();
-            prev->next=temp;
-            sum=0;
-            sum+=rem;
-            if(curr1!=NULL)
-            {
-                sum+=curr1->val;
-                curr1=curr1->next;
-            }
-            if(curr2!=NULL)
-            {
-                sum+=curr2->val;
-                curr2=curr2->next;
-            }
-            if(sum<=9)
-                temp->val=sum;
-            else 
-                temp->val=sum%10;
-            prev=temp;
-            if(sum>9)
-                rem=sum/10;
-            else
-                rem=0;
-        }
-        if(rem!=0)
+        ListNode *dummy=new ListNode();
+        ListNode *temp=dummy;
+        int carry=0;
+        while((l1!=NULL || l2!=NULL) || carry)
         {
-            ListNode *temp=new ListNode();
-            temp->val=rem;
-            prev->next=temp;
+            int sum=0;
+            if(l1!=NULL)
+            {
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2!=NULL)
+            {
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            sum+=carry;
+            carry=sum/10;
+            ListNode *node=new ListNode(sum%10);
+            temp->next=node;
+            temp=temp->next;
         }
-        return head->next;
+        return dummy->next;
     }
 };

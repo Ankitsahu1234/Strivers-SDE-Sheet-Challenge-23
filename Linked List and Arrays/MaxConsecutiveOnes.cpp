509d773196c429f15538f067ecb076d19e0ec440
Ankit Sahu <ankitsahu2032@gmail.com>
2023-08-07 22:52:30 +0530

    Update MaxConsecutiveOnes.cpp

--- Linked List and Arrays/MaxConsecutiveOnes.cpp
+++ Linked List and Arrays/MaxConsecutiveOnes.cpp
@@ -3,18 +3,16 @@ public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
         
         int n=nums.size();
-        int max_so_far=0;
-        int curr_max=0;
+        int count=0;
+        int maxi=0;
         for(int i=0; i<n; i++)
         {
             if(nums[i]==1)
-            {
-                curr_max=curr_max+1;
-                max_so_far=max(max_so_far, curr_max);
-            }
+                count++;
             else 
-                curr_max=0;
+                count=0;
+            maxi=max(maxi, count);
         }
-        return max_so_far;
+        return maxi;
     }
 };

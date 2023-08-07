728fbd3c73f95d09d0ecea0f37045c79766a4c1e
Ankit Sahu <ankitsahu2032@gmail.com>
2023-08-07 21:49:23 +0530

    Update 3Sum.cpp

--- Linked List and Arrays/3Sum.cpp
+++ Linked List and Arrays/3Sum.cpp
@@ -2,32 +2,38 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         
-        int n=nums.size();
+        // Method 3: Efficient
+        
         vector<vector<int>> res;
+        int n=nums.size();
         sort(nums.begin(), nums.end());
-        set<vector<int>> myset;
         for(int i=0; i<n; i++)
         {
-            int left=i+1;
-            int right=n-1;
-            while(left<right)
+            if(i!=0 && nums[i]==nums[i-1])
+                continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k)
             {
-                int sum=nums[i]+nums[left]+nums[right];
+                int sum=nums[i]+nums[j]+nums[k];
                 if(sum<0)
-                    left++;
+                    j++;
                 else if(sum>0)
-                    right--;
+                    k--;
                 else
                 {
-                    vector<int> temp={nums[i], nums[left], nums[right]};
-                    if(myset.find(temp)==myset.end())
-                        res.push_back(temp);
-                    myset.insert(temp);
-                    left++;
-                    right--;
+                    vector<int> temp={nums[i], nums[j], nums[k]};
+                    res.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])
+                        j++;
+                    while(j<k && nums[k]==nums[k+1])
+                        k--;
                 }
             }
         }
         return res;
     }
 };
+

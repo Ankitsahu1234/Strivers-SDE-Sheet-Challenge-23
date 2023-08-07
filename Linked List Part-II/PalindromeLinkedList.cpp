6688b0b560a270652ae1901f693a9b753615b3de
Ankit Sahu <ankitsahu2032@gmail.com>
2023-08-07 17:15:40 +0530

    Update PalindromeLinkedList.cpp

--- Linked List Part-II/PalindromeLinkedList.cpp
+++ Linked List Part-II/PalindromeLinkedList.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
-    bool isPal(vector<int> &temp)
+
+    ListNode* reverse(ListNode *node)
     {
-        int left=0, right=temp.size()-1;
-        while(left<right)
+        ListNode *prev=NULL, *curr=node, *nxt;
+        while(curr!=NULL)
         {
-            if(temp[left]!=temp[right])
-                return false;
-            left++;
-            right--;
+            nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
         }
-        return true;
+        return prev;
     }
 
     bool isPalindrome(ListNode* head) {
         
-        vector<int> temp;
-        ListNode *curr=head;
-        while(curr!=NULL)
-        {
-            temp.push_back(curr->val);
-            curr=curr->next;
-        }
-        if(isPal(temp))
+        if(head==NULL || head->next==NULL)
             return true;
-        return false;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        slow->next=reverse(slow->next);
+        slow=slow->next;
+        ListNode *dummy=head;
+        while(slow!=NULL)
+        {
+            if(slow->val!=dummy->val)
+                return false;
+            slow=slow->next;
+            dummy=dummy->next;
+        }
+        return true;
     }
 };

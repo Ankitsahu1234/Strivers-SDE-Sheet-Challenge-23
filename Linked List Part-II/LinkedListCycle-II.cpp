33acb521edc827a4d166a7a85042e314219682ac
Ankit Sahu <ankitsahu2032@gmail.com>
2023-08-07 17:23:41 +0530

    Update LinkedListCycle-II.cpp

--- Linked List Part-II/LinkedListCycle-II.cpp
+++ Linked List Part-II/LinkedListCycle-II.cpp
@@ -2,14 +2,24 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         
-        unordered_set<ListNode*> myset;
-        ListNode *curr=head;
-        while(curr!=NULL)
+        if(head==NULL || head->next==NULL)
+            return NULL;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        ListNode *entry=head;
+        while(fast->next!=NULL && fast->next->next!=NULL)
         {
-            if(myset.find(curr)!=myset.end())
-                return curr;
-            myset.insert(curr);
-            curr=curr->next;
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            {
+                while(slow!=entry)
+                {
+                    slow=slow->next;
+                    entry=entry->next;
+                }
+                return slow;
+            }
         }
         return NULL;
     }

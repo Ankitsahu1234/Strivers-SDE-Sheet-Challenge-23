df70bd604d619e3bcaff41e88dc7483410ecdc67
Ankit Sahu <ankitsahu2032@gmail.com>
2023-08-07 18:01:16 +0530

    Update RotateALinkedList.cpp

--- Linked List and Arrays/RotateALinkedList.cpp
+++ Linked List and Arrays/RotateALinkedList.cpp
@@ -2,35 +2,26 @@ class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         
-        if(head==NULL || head->next==NULL)
+        // Method 3: Efficient
+        
+        if(head==NULL || head->next==NULL || k==0)
             return head;
-        ListNode *curr=head;
-        int len=0;
-        while(curr!=NULL)
+        ListNode *temp=head;
+        int len=1;
+        while(temp->next!=NULL)
         {
-            curr=curr->next;
             len++;
+            temp=temp->next;
         }
+        temp->next=head;
         k=k%len;
-        if(k==0)
-            return head;
-        int count=0;
-        ListNode *end=head;
-        ListNode *ans=head->next;
-        ListNode *last=head;
-        while(count<k)
+        int end=len-k;
+        while(end--)
         {
-            end=end->next;
-            count++;
+            temp=temp->next;
         }
-        while(end->next!=NULL)
-        {
-            end=end->next;
-            ans=ans->next;
-            last=last->next;
-        }
-        end->next=head;
-        last->next=NULL;
-        return ans;
+        head=temp->next;
+        temp->next=NULL;
+        return head;
     }
 };

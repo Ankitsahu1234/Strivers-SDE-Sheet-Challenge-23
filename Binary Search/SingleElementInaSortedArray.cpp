259b0b08e7b3639667a04cb44fb03b6ce894d2b1
Ankit Sahu <ankitsahu2032@gmail.com>
2023-08-11 15:00:49 +0530

    Update SingleElementInaSortedArray.cpp

--- Binary Search/SingleElementInaSortedArray.cpp
+++ Binary Search/SingleElementInaSortedArray.cpp
@@ -1,13 +1,49 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-
-        // Method 2: Using XOR
-        int res=0;
-        for(int i=0; i<nums.size(); i++)
+        
+        int n=nums.size();
+        // Edge cases
+        if(n==1)
+            return nums[0];
+        if(nums[0]!=nums[1])
+            return nums[0];
+        if(nums[n-1]!=nums[n-2])
+            return nums[n-1];
+        int low=1, high=n-2;
+        while(low<=high)
         {
-            res=res^nums[i];
+            int mid=(low+high)/2;
+            // If nums[mid] is the single element
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
+                return nums[mid];
+            
+            // We are in the left:
+            if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]))
+            {
+                // Eliminate the left half
+                low=mid+1;
+            }
+            // We are in the right:
+            else
+            {
+                high=mid-1;
+            }
         }
-        return res;
+        return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+
+//         // Method 2: Using XOR
+//         int res=0;
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             res=res^nums[i];
+//         }
+//         return res;
+//     }
+// };

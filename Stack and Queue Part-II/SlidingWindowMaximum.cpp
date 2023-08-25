2186cae95f34c960ba80be4c66ee16c8fdff0613
Ankit Sahu <ankitsahu2032@gmail.com>
2023-08-25 14:50:04 +0530

    Create SlidingWindowMaximum.cpp

---
+++ Stack and Queue Part-II/SlidingWindowMaximum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        // Method 1: Using Priority Queue (max heap) for pair(nums[i], i)
+        
+        int n=nums.size();
+        vector<int> res(n-k+1);
+        priority_queue<pair<int,int>> pq;
+        for(int i=0; i<k; i++)
+        {
+            pq.push({nums[i], i});
+        }
+        res[0]=pq.top().first;
+        for(int i=k; i<n; i++)
+        {
+            while(!pq.empty() && pq.top().second<=(i-k))
+                pq.pop();
+            pq.push({nums[i], i});
+            res[i-k+1]=pq.top().first;
+        }
+        return res;
+    }
+};

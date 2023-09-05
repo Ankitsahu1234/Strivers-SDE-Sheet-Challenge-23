c8e68cf8d12bbba7b53353fcf147a14f88aa2dff
Ankit Sahu <ankitsahu2032@gmail.com>
2023-09-05 14:41:43 +0530

    Update RepeatedStringMatch.cpp

--- String/RepeatedStringMatch.cpp
+++ String/RepeatedStringMatch.cpp
@@ -1,29 +1,52 @@
 class Solution {
 public:
-    int repeatedStringMatch(string a, string b) {
+    int repeatedStringMatch(string A, string B) {
         
-        set<int> myset;
-        for(char ch: a)
-            myset.insert(ch);
-        for(char ch: b)
+        // Method 2:
+
+        for(int i=0; i<A.size(); i++)
         {
-            if(myset.find(ch)==myset.end())
-                return -1;
-        }
-        string temp=a;
-        int res=1;
-        while(1)
-        {
-            if(temp.find(b)!=string::npos)
-                return res;
-            else if(temp.length()>12*b.length())
-                return -1;
-            else
+            int j;
+            for(j=0; j<B.size(); )
             {
-                temp+=a;
-                res++;
+                if(A[(i+j)%A.size()]==B[j])
+                    j++;
+                else
+                    break;
             }
+            if(j==B.size())
+                return ((i+B.size()-1)/A.size())+1;
         }
-        return res;
+        return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int repeatedStringMatch(string a, string b) {
+        
+//         set<int> myset;
+//         for(char ch: a)
+//             myset.insert(ch);
+//         for(char ch: b)
+//         {
+//             if(myset.find(ch)==myset.end())
+//                 return -1;
+//         }
+//         string temp=a;
+//         int res=1;
+//         while(1)
+//         {
+//             if(temp.find(b)!=string::npos)
+//                 return res;
+//             else if(temp.length()>12*b.length())
+//                 return -1;
+//             else
+//             {
+//                 temp+=a;
+//                 res++;
+//             }
+//         }
+//         return res;
+//     }
+// };

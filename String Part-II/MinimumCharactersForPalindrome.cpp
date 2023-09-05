e3f0ec733f64180e0bbd0be0e49359b65bbc6700
Ankit Sahu <ankitsahu2032@gmail.com>
2023-09-05 17:29:37 +0530

    Create MinimumCharactersForPalindrome.cpp

---
+++ String Part-II/MinimumCharactersForPalindrome.cpp
@@ -0,0 +1,25 @@
+int minCharsforPalindrome(string str) {
+	// Write your code here.
+	
+	// Method 2: 
+	
+	int i=0, j=str.length()-1;
+	int count=0;
+	int k=j;
+	while(i<=j)
+	{
+		if(str[i]==str[j])
+		{
+			i++;
+			j--;
+		}
+		else
+		{
+			i=0;
+			k--;
+			j=k;
+			count++;
+		}
+	}
+	return count;
+}

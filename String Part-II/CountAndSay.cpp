b270aa598fe9d92a1c86b93affe7d443a1726464
Ankit Sahu <ankitsahu2032@gmail.com>
2023-09-06 22:01:06 +0530

    Create CountAndSay.cpp

---
+++ String Part-II/CountAndSay.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        
+        if(n==1)
+            return "1";
+        if(n==2)
+            return "11";
+        string str="11";
+        for(int i=3; i<=n; i++)
+        {
+            string temp="";
+            str=str+"$";
+            int count=1;
+            for(int j=1; j<str.length(); j++)
+            {
+                if(str[j]==str[j-1])
+                    count++;
+                else
+                {
+                    temp=temp+to_string(count)+str[j-1];
+                    count=1;
+                }
+            }
+            str=temp;
+        }
+        return str;
+    }
+};

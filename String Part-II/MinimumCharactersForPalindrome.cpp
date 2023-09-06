92c80ff146081a05e74b1abfc38149c126061dd3
Ankit Sahu <ankitsahu2032@gmail.com>
2023-09-06 21:06:24 +0530

    Update MinimumCharactersForPalindrome.cpp

--- String Part-II/MinimumCharactersForPalindrome.cpp
+++ String Part-II/MinimumCharactersForPalindrome.cpp
@@ -1,25 +1,68 @@
-int minCharsforPalindrome(string str) {
-	// Write your code here.
-	
-	// Method 2: 2 Pointer approach
-	
-	int i=0, j=str.length()-1;
-	int count=0;
-	int k=j;
-	while(i<=j)
+#include<bits/stdc++.h>
+
+vector<int> calculateLPSArray(string str)
+{
+	int m=str.length();
+	vector<int> lps(m);
+	int len=0;
+	lps[0]=0;
+	int i=1;
+	while(i<m)
 	{
-		if(str[i]==str[j])
+		if(str[i]==str[len])
 		{
+			len++;
+			lps[i]=len;
 			i++;
-			j--;
 		}
 		else
 		{
-			i=0;
-			k--;
-			j=k;
-			count++;
+			if(len!=0)
+				len=lps[len-1];
+			else
+			{
+				lps[i]=0;
+				i++;
+			}
 		}
 	}
-	return count;
+	return lps;
 }
+
+int minCharsforPalindrome(string str) {
+	// Write your code here.
+	
+	// Method 3: Using "LPS array" 
+
+	string revStr=str;
+	reverse(revStr.begin(), revStr.end());
+	string concat=str+"$"+revStr;
+	vector<int> lps=calculateLPSArray(concat);
+	return (str.length()-lps[lps.size()-1]);
+}
+
+// int minCharsforPalindrome(string str) {
+// 	// Write your code here.
+	
+// 	// Method 2: 2 Pointer approach
+	
+// 	int i=0, j=str.length()-1;
+// 	int count=0;
+// 	int k=j;
+// 	while(i<=j)
+// 	{
+// 		if(str[i]==str[j])
+// 		{
+// 			i++;
+// 			j--;
+// 		}
+// 		else
+// 		{
+// 			i=0;
+// 			k--;
+// 			j=k;
+// 			count++;
+// 		}
+// 	}
+// 	return count;
+// }

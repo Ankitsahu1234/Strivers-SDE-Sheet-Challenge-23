f68a509ebbc664f5e5c2e3c10d11baeaa7f90e3b
Ankit Sahu <ankitsahu2032@gmail.com>
2023-09-07 00:28:04 +0530

    Update PreorderTraversal.cpp

--- Binary Tree/PreorderTraversal.cpp
+++ Binary Tree/PreorderTraversal.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
 
-    void inorder(TreeNode *root, vector<int> &res)
+    void preorder(TreeNode *root, vector<int> &res)
     {
         if(root==NULL)
             return;
         res.push_back(root->val);
-        inorder(root->left, res);
-        inorder(root->right, res);
+        preorder(root->left, res);
+        preorder(root->right, res);
     }
 
     vector<int> preorderTraversal(TreeNode* root) {
         
         vector<int> res;
-        inorder(root, res);
+        preorder(root, res);
         return res;
     }
 };

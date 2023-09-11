c8339d88a161f0f41d989d5e9a3c45fe8911f234
Ankit Sahu <ankitsahu2032@gmail.com>
2023-09-11 17:31:40 +0530

    Update ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp

--- Binary Tree part-III/ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp
+++ Binary Tree part-III/ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp
@@ -1,22 +1,66 @@
 class Solution {
-    TreeNode* buildHelper(vector<int> &preorder, vector<int> &inorder, int &rootIdx, int left, int right)
-    {
-        if(left>right)
-            return NULL;
-        int pivot=left;
-        while(inorder[pivot]!=preorder[rootIdx])
-            pivot++;
-        rootIdx++;
-        TreeNode *newNode=new TreeNode(inorder[pivot]);
-        newNode->left=buildHelper(preorder, inorder, rootIdx, left, pivot-1);
-        newNode->right=buildHelper(preorder, inorder, rootIdx, pivot+1, right);
-        return newNode;
-    }
-
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
         
-        int rootIdx=0;
-        return buildHelper(preorder, inorder, rootIdx, 0, inorder.size()-1);
+        // Method 2: Iterative Solution
+
+        int i=0, j=0;
+        TreeNode *root=new TreeNode(0x80000000);
+        TreeNode *pp=NULL, *ptr=root;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(j<in.size())
+        {
+            if(st.top()->val==in[j])
+            {
+                pp=st.top();
+                st.pop();
+                j++;
+            }
+            else if(pp!=NULL)
+            {
+                ptr=new TreeNode(pre[i]);
+                pp->right=ptr;
+                pp=NULL;
+                st.push(ptr);
+                i++;
+            }
+            else
+            {
+                ptr=new TreeNode(pre[i]);
+                st.top()->left=ptr;
+                st.push(ptr);
+                i++;
+            }
+        }
+        ptr=root->left;
+        delete root;
+        return ptr;
     }
 };
+
+
+// class Solution {
+//     TreeNode* buildHelper(vector<int> &preorder, vector<int> &inorder, int &rootIdx, int left, int right)
+//     {
+//         if(left>right)
+//             return NULL;
+//         int pivot=left;
+//         while(inorder[pivot]!=preorder[rootIdx])
+//             pivot++;
+//         rootIdx++;
+//         TreeNode *newNode=new TreeNode(inorder[pivot]);
+//         newNode->left=buildHelper(preorder, inorder, rootIdx, left, pivot-1);
+//         newNode->right=buildHelper(preorder, inorder, rootIdx, pivot+1, right);
+//         return newNode;
+//     }
+
+// public:
+//     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        
+//         int rootIdx=0;
+//         return buildHelper(preorder, inorder, rootIdx, 0, inorder.size()-1);
+//     }
+// };
